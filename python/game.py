27dc9cba7abdda9f0d6af6f541e825204e7057d9
Laura <laura.deruwez@lyceedesflandres.fr>
2024-02-26 12:21:55 +0100

    Game me semble complété

--- python/game.py
+++ python/game.py
@@ -2,20 +2,55 @@ from maze import Maze
 
 
 class Game:
-    # Classe avec une instance de maze et un joueur représenté en coordonénées x et y
+    """Class with a maze instance and a player represented in x and y coordinates"""
+
     def __init__(self, width: int, height: int):
         self.maze = Maze(width, height)
         self.player = (0, 0)
 
-    # Méthode pour afficher le labyrinthe
     def display(self):
-        # Doit renvoyer une chaîne de caractères représentant le labyrinthe avec le joueur à toi de voir comment tu veux faire passer ça pour ensuite facilement le traiter en JS (peut-être te renseigner sur le format json)
-        return
+        """A : Il n'y a aucun mur
+        B : Il y a un mur à droite et rien en dessous
+        C : Il n'y a pas de murs à droite et un en dessous
+        D : Il y a deux murs"""
+        cells = ""
+        for cell in self.maze.cells:
+            if (cell, cell.neighbors[2]) not in self.maze.walls and (
+                cell,
+                cell.neighbors[3],
+            ) not in self.maze.walls:
+                cells += "A"
+            elif (cell, cell.neighbors[2]) in self.maze.walls and (
+                cell,
+                cell.neighbors[3],
+            ) not in self.maze.walls:
+                cells += "B"
+            elif (cell, cell.neighbors[2]) not in self.maze.walls and (
+                cell,
+                cell.neighbors[3],
+            ) in self.maze.walls:
+                cells += "C"
+            else:
+                cells += "D"
+        return {
+            "laby": cells,
+            "pos_player": str(self.player),
+            "taille": str(self.maze.width),
+            "state": self.has_won(),
+        }
 
-    # Méthode pour déplacer le joueur
     def move(self, direction: str):
-        return
+        x, y = self.player
+        if direction == "up":
+            self.player = (x, y - 1)
+        elif direction == "down":
+            self.player = (x, y + 1)
+        elif direction == "left":
+            self.player = (x + 1, y)
+        else:
+            self.player = (x - 1, y)
 
-    # Méthode pour vérifier si le joueur a gagné
     def has_won(self) -> bool:
-        return
+        if self.player == (self.maze.width, self.maze.height):
+            return True
+        return False
